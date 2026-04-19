0f7d95f9af43cf1dfe81ba0a1eb6d24bb49da684
tatus <tatusolavi@gmail.com>
2026-04-19 17:31:40 +0300

    Started on the newsletter functionality, doenst work yet but taking a break.

--- Toolkit_API/Application/Interfaces/IEmailServices.cs
+++ Toolkit_API/Application/Interfaces/IEmailServices.cs
@@ -2,5 +2,7 @@
 {
     public interface IEmailServices
     {
+        public Task<string> SendEmail(string to, string subject, string body);
+        public Task<string> SubscribeToNewsLetter(string email);
     }
 }
--- Toolkit_API/Infrastructure/Services/EmailServices.cs
+++ Toolkit_API/Infrastructure/Services/EmailServices.cs
@@ -1,6 +1,39 @@
-﻿namespace Toolkit_API.Infrastructure.Services
+﻿using Toolkit_API.Application.Interfaces;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+namespace Toolkit_API.Infrastructure.Services
 {
-    public class EmailServices
+    public class EmailServices : IEmailServices
     {
+        public async Task<string> SendEmail(string to, string subject, string body)
+        {
+            var msg = new MimeMessage();
+
+            msg.From.Add(new MailboxAddress("Avtoolkit News Letter", "avtoolkitnews@gmail.com"));
+
+            msg.To.Add(new MailboxAddress("", to));
+
+            msg.Subject = subject;
+            msg.Body = new TextPart("plain")
+            {
+                Text = body
+            };
+
+            using(var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+                await client.AuthenticateAsync("avtoolkitnews@gmail.com", Environment.GetEnvironmentVariable("NEWS_PS"));
+                await client.SendAsync(msg);
+                await client.DisconnectAsync(true);
+            }
+
+            return "Email sent successfully";
+        }
+        public async Task<string> SubscribeToNewsLetter(string email)
+        {
+            // Logic to add the email to the subscription list
+            return "Subscribed successfully";
+        }
     }
 }
