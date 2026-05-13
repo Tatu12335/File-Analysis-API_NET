8fd137942b60f79f33151551bb4c02e7b727149e
tatus <tatusolavi@gmail.com>
2026-05-13 13:38:04 +0300

    Continued on the jobqueue but its pretty hard.

--- Toolkit_API/Domain/Entities/Files/FolderInfo.cs
+++ Toolkit_API/Domain/Entities/Files/FolderInfo.cs
@@ -1,10 +1,8 @@
-﻿using Org.BouncyCastle.Asn1.Mozilla;
-
-namespace Toolkit_API.Domain.Entities.Files
+﻿namespace Toolkit_API.Domain.Entities.Files
 {
     public class FolderInfo
     {
         public List<string> Files { get; set; } = new List<string>();
-        
+
     }
 }
